a9a8802506f45f9bdef7383463626e1bf7013c57
isurudotxyz <agisuru@icloud.com>
2023-08-24 10:30:40 +0200

    data formatting

--- pages/stories.js
+++ pages/stories.js
@@ -1,10 +1,15 @@
+import Story from "../component/Story.js";
 import view from "../utils/view.js";
 export default async function Stories(path) {
   const stories = await getStories(path);
   const hasStories = stories.length > 0;
 
   view.innerHTML = `<div>${
-    hasStories ? stories.map((story) => JSON.stringify(story)) : "No stories"
+    hasStories
+      ? stories
+          .map((story, index) => Story({ ...story, index: index + 1 }))
+          .join("")
+      : "No stories"
   } </div>`;
 }
 
